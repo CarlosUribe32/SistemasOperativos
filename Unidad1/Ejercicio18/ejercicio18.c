023ca1abed9d4102b662bdf9fc897eaf6cc26445
CarlosUribe32 <carlos.uribec@upb.edu.co>
2021-01-26 17:24:22 -0500

    Ejercicio 18 Esta vez si es la ultima

--- Unidad1/Ejercicio18/ejercicio18.c
+++ Unidad1/Ejercicio18/ejercicio18.c
@@ -7,20 +7,23 @@ int main(void){
     int pos2;
     printf("Ingrese el numero de elementos del arreglo\n");
     scanf("%d[^\n]", &pos);
+    getc(stdin);
     char array [pos];
     for (int i = 0; i < pos; i++)
     {
         printf("Ingrese la posicion %d\n", i+1);
         scanf("%s[^\n]", (array+i));
+        getc(stdin);
     }
     contenido(array, pos);
     printf("Ingrese la posicion que quiere cambiar\n");
     scanf("%d[^\n]", &pos2);
+    getc(stdin);
     if(pos2>=1 && pos2<=pos){
         printf("Ingrese el valor\n");
-        char *a = array+pos2-1;
-        scanf("%s[^\n]", a);
-        contenido(array, pos+1);
+        scanf("%c", (array+pos2-1));
+        getc(stdin);
+        contenido(array, pos);
     }
     else
         printf("Esta posicion no es valida\n");
