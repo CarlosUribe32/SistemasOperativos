62256230b8865e267ff1cf5722e560152b28b3a3
CarlosUribe32 <carlos.uribec@upb.edu.co>
2021-02-03 13:55:40 -0500

    Cuadrando el mkdb y el loaddb del proyecto 1

--- Unidad1/Proyecto1/bd.c
+++ Unidad1/Proyecto1/bd.c
@@ -9,24 +9,91 @@ typedef struct{
 }estudiante;
 
 typedef struct{
-    char nombre[20];
+    char nombre[30];
     int numRegistros;
+    int size;
     estudiante *registro;
 }bdActual;
 
+char checkOf(FILE *archivo){
+    char ch;
+    ch = fgetc(archivo);
+    ch = fgetc(archivo);
+    ch = fgetc(archivo);
+    ch = fgetc(archivo);
+    return ch;
+}
+
+bdActual * mkdb(char[30], int);
+bdActual *loaddb(FILE *);
+
 int main(void){
+
+    bdActual *pbd;
     char comando [10];
-    char par1[10];
+    char par1[30];
     int par2;
     int par3;
     do
     {
         printf("Ingrese el comando:\n");
         scanf("%s", comando);
-        printf("El com es: %s\n", comando);
-        fscanf(stdin, "%s", par1);
-        printf("Goleee con estas papa %s\n", par1);
+        if(strcmp(comando, "mkdb")==0)
+        {
+            fscanf(stdin, "%s %d", par1, &par2);
+            pbd = mkdb(par1, par2);
+            printf("Se contruyo con exito la BD %s\n", par1);
+        }
+        else if (strcmp(comando, "loaddb")==0){
+            fscanf(stdin, "%s", par1);
+            FILE *in_file = fopen(par1, "r");
+            if(in_file == NULL)
+                printf("No se puede abrir %s\n", par1);
+            else{
+
+            }
+        }
+        else if (strcmp(comando, "savedb")==0){
+
+        }
+        else if (strcmp(comando, "readall")==0){
+
+        }
+        else if (strcmp(comando, "readsize")==0){
+
+        }
+        else if (strcmp(comando, "mkreg")==0){
+
+        }
+        else if (strcmp(comando, "readreg")==0){
+
+        }
+        getc(stdin);
     } while (strcmp(comando, "exit")!=0);
     
     return 0;
+}
+
+bdActual * mkdb(char name[30], int size){
+    bdActual bd = {name, size, 0, (estudiante *)malloc(sizeof(estudiante)*size)};
+    bdActual *pbd = &bd;
+    return pbd;
+}
+
+bdActual *loaddb(FILE *in_file){
+    char ch;
+    char name[30], people[30];
+    int i=0, size, ced, sem;
+    fscanf(in_file, "%s %d", name, &size);
+    bdActual bd = {name, size, 0, (estudiante *)malloc(sizeof(estudiante)*size)};
+    while(ch!=EOF){
+        fscanf(in_file, "%d %s %d", ced, people , sem);
+        bd.registro+i-->ced;
+        bd.registro+i-->people;
+        bd.registro+i-->sem;
+        i++;
+        ch = checkOf(in_file);
+    }
+    bd.size = i;
+    return &bd;
 }
