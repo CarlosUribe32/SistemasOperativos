65d976b86713fd2dfe494914087e186f9501f969
CarlosUribe32 <carlos.uribec@upb.edu.co>
2021-02-12 07:48:46 -0500

    Ejercicio 63

--- Unidad1/Ejercicio62/main2.c
+++ Unidad1/Ejercicio62/main2.c
@@ -2,6 +2,7 @@
  #include <stdlib.h>
 
  int main(int argc, char* argv[]) {
-     char *ptr = malloc(20*sizeof(char));
+     char *ptr = (char *)malloc(20*sizeof(char));
+     free(ptr);
      return 0;
  }
